42890ef80e7ddba9166d8aea5505175571de88df
Prakashjangid429 <157800296+Prakashjangid429@users.noreply.github.com>
2025-08-26 10:36:43 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,23 @@ import type React from "react"
 import type { Metadata } from "next"
 import "./globals.css"
 import { AuthProvider } from "@/lib/auth-context"
+import { Montserrat, Open_Sans } from "next/font/google"
 import { Navigation } from "@/components/navigation"
 import { Footer } from "@/components/footer"
 
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-montserrat",
+  weight: ["400", "600", "700", "900"],
+})
+
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-open-sans",
+  weight: ["400", "500", "600"],
+})
 
 export const metadata: Metadata = {
   title: "Spiral Pay - Modern Payment Gateway Solutions",
@@ -20,19 +34,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <style>
-          {`
-@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-            @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
-            @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-            body {
-              font-family:'Montserrat','Open Sans';
-              font-weight:400;
-            }
-          `}
-        </style>
+        <style>{`
+html {
+  font-family: ${openSans.style.fontFamily};
+  --font-sans: ${openSans.variable};
+  --font-heading: ${montserrat.variable};
+}
+        `}</style>
       </head>
-      <body className={`antialiased`}>
+      <body className={`${montserrat.variable} ${openSans.variable} className={`antialiased`}>
         <AuthProvider>
           <Navigation />
           <main>{children}</main>
